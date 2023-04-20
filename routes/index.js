f6a4c9d21f3de3479197b615c3005cfe4f84dbac
Kashish Goyal <kashishgoyal961@gmail.com>
2023-04-20 11:19:46 +0530

    auth done

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,52 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../app/models/user_schema');
+const passport = require('passport');
+
 
 router.get('/', function (req, res) {
     return res.render('home');
-})
-router.get('/login', function (req, res) {
-    res.render('./auth/workers/login');
-})
+});
+
+
 
 router.get('/register', function (req, res) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
+    }
     res.render('./auth/workers/register');
-})
+});
 
 router.post('/register', async function (req, res) {
-    console.log(req.body);
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
-        const newUser = User.create(req.body);
+        const newUser = await User.create(req.body);
         console.log(newUser);
+        return res.redirect('./login');
     }
     else {
         return res.render('./auth/workers/register', { error: 'Email already exists' });
     }
 });
 
+
+router.get('/login', function (req, res) {
+    if (req.isAuthenticated()) {
+        return res.redirect('/');
+    }
+    res.render('./auth/workers/login');
+});
+
+
+
+router.post('/login', passport.authenticate(
+    'local',
+    {
+        failureRedirect: '/login',
+    }
+), function (req, res) {
+    console.log("session connected");
+    return res.redirect('/');
+})
 
 module.exports = router;
